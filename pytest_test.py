5dd053af0a40f03aee7b08d376735c7f8eed55eb
sachinbhuibar <sachinbhuibar@gmail.com>
2021-09-30 13:06:17 +0530

    initial commit

---
+++ pytest_test.py
@@ -0,0 +1,4 @@
+import pytest
+
+def test_method():
+  assert 2==2
---
+++ second-app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+secondapp = Flask(__name__)
+
+@secondapp.route("/")
+def index():
+	return "second service on port 4000"
+
+if __name__ == "__main__":
+	secondapp.run(host="0.0.0.0", port = 4000)
