dcad0491988390e8228fcfb09a95a33268c9c5ab
sachinbhuibar <sachinbhuibar@gmail.com>
2021-10-14 11:17:17 +0530

    rectify code for pycodestyle and add code style check in Jenkinsfile.build

--- second-app.py
+++ second-app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 secondapp = Flask(__name__)
 
+
 @secondapp.route("/")
 def index():
-	return "second service on port 4000"
+    return "second service on port 4000"
+
 
 if __name__ == "__main__":
-	secondapp.run(host="0.0.0.0", port = 4000)
+    secondapp.run(host="0.0.0.0", port=4000)
