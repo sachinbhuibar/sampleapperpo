9360e5ac6818caa490f25f3137b1154b869e5cbc
sachinbhuibar <sachinbhuibar@gmail.com>
2021-10-14 11:11:29 +0530

    rectify code for pycodestyle

--- first-app.py
+++ first-app.py
@@ -4,7 +4,8 @@ firstapp = Flask(__name__)
 
 @firstapp.route("/")
 def index():
-	return "first service on port 3000"
+    return "first service on port 3000"
+
 
 if __name__ == "__main__":
-	firstapp.run(host = "0.0.0.0", port = 3000)
+    firstapp.run(host="0.0.0.0", port=3000)
